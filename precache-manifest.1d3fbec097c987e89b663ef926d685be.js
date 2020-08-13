2b8d7ba9d9eb8af853c88e9a918048735bda770b
anniephilip23 <anniephilip23@gmail.com>
2020-08-13 23:51:25 +0530

    Updates

---
+++ precache-manifest.1d3fbec097c987e89b663ef926d685be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b675a3fb6897d64c7b115e257b27e7e",
+    "url": "/weassist/index.html"
+  },
+  {
+    "revision": "5ae172ec22f6f1ec7576",
+    "url": "/weassist/static/css/2.a70ede4d.chunk.css"
+  },
+  {
+    "revision": "15a5146456f908ae5f84",
+    "url": "/weassist/static/css/main.445e86b3.chunk.css"
+  },
+  {
+    "revision": "5ae172ec22f6f1ec7576",
+    "url": "/weassist/static/js/2.80581518.chunk.js"
+  },
+  {
+    "revision": "9b01cb500fd0a017181399e304cf1f67",
+    "url": "/weassist/static/js/2.80581518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a5146456f908ae5f84",
+    "url": "/weassist/static/js/main.07d91ecc.chunk.js"
+  },
+  {
+    "revision": "78536c4b813d8ec4915e",
+    "url": "/weassist/static/js/runtime-main.d58e41bb.js"
+  },
+  {
+    "revision": "efbe959fbae3570a9406093254765568",
+    "url": "/weassist/static/media/logo.efbe959f.png"
+  }
+]);
